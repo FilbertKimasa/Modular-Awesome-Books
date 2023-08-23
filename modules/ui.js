9dd9446cd3caafc83d0d6f5a1f5b7619dd4f7bec
FilbertKimasa <filbertdan67@gmail.com>
2023-08-23 19:22:34 +0300

    Add ui module

---
+++ modules/ui.js
@@ -0,0 +1,21 @@
+// uiModule.js
+export default class UI {
+  constructor() {
+    this.bookTitle = document.getElementById('book-title');
+    this.bookAuthor = document.getElementById('book-author');
+    this.addBtn = document.getElementById('btn');
+    this.listEntry = document.getElementById('list-entry');
+    this.listLink = document.querySelector('#bk-list');
+    this.addLink = document.querySelector('#bk-add');
+    this.listContact = document.querySelector('#bk-contact');
+    this.sectionList = document.querySelector('#list');
+    this.sectionNew = document.querySelector('#new');
+    this.sectionContact = document.querySelector('#contact');
+    this.showDate = document.querySelector('.show-date');
+  }
+
+  clearInputs() {
+    this.bookTitle.value = '';
+    this.bookAuthor.value = '';
+  }
+}
